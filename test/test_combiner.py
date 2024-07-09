bde8c882e2bc44a24d1f193ccc44ee868a560614
Felix Dangel <felix.dangel@vectorinstitute.ai>
2024-07-09 11:29:59 -0400

    [ADD] Pre-conditioner group for jointly treating Linear weight+bias

--- test/test_combiner.py
+++ test/test_combiner.py
@@ -3,10 +3,10 @@
 from test.utils import DEVICE_IDS, DEVICES
 
 from pytest import mark
-from torch import allclose, device, manual_seed, rand
+from torch import allclose, device, manual_seed, rand, zeros
 from torch.nn import Linear
 
-from sirfshampoo.combiner import PerParameter
+from sirfshampoo.combiner import LinearWeightBias, PerParameter
 
 
 @mark.parametrize("dev", DEVICES, ids=DEVICE_IDS)
@@ -57,3 +57,51 @@ def test_PerParameter_group_and_ungroup(dev: device):
     assert allclose(grouped, scalar)
     (ungrouped,) = PerParameter().ungroup(grouped, shapes)
     assert allclose(ungrouped, scalar_unsqueezed)
+
+
+@mark.parametrize("dev", DEVICES, ids=DEVICE_IDS)
+def test_LinearWeightBias_identify(dev: device):
+    """Test parameter identification of `LinearWeightBias` class.
+
+    Args:
+        dev: Device to run the test on.
+    """
+    model = Linear(2, 3).to(dev)
+    assert LinearWeightBias().identify(model) == [[model.weight, model.bias]]
+
+
+@mark.parametrize("dev", DEVICES, ids=DEVICE_IDS)
+def test_LinearWeightBias_group_and_ungroup(dev: device):
+    """Test tensor (un-)grouping of `LinearWeightBias` class.
+
+    Args:
+        dev: Device to run the test on.
+    """
+    manual_seed(0)
+
+    # regular shape
+    D_out, D_in = 5, 3
+    W, b = rand(D_out, D_in, device=dev), rand(D_out, device=dev)
+    tensor = zeros(D_out, D_in + 1, device=dev)
+    tensor[:, :D_in] = W
+    tensor[:, D_in] = b
+    shapes = [W.shape, b.shape]
+
+    grouped = LinearWeightBias().group([W, b])
+    assert allclose(grouped, tensor)
+    (W_ungrouped, b_ungrouped) = LinearWeightBias().ungroup(grouped, shapes)
+    assert allclose(W_ungrouped, W) and allclose(b_ungrouped, b)
+
+    # one input and output dimension
+    D_out, D_in = 1, 1
+    W, b = rand(D_out, D_in, device=dev), rand(D_out, device=dev)
+    tensor = zeros(D_out, D_in + 1, device=dev)
+    tensor[:, :D_in] = W
+    tensor[:, D_in] = b
+    tensor = tensor.squeeze(0)
+    shapes = [W.shape, b.shape]
+
+    grouped = LinearWeightBias().group([W, b])
+    assert allclose(grouped, tensor)
+    (W_ungrouped, b_ungrouped) = LinearWeightBias().ungroup(grouped, shapes)
+    assert allclose(W_ungrouped, W) and allclose(b_ungrouped, b)
